fb8a1f647fcabb9ee3e30c4653eb368275ae04a8
nyhave <nyhave@gmail.com>
2025-08-21 14:39:17 +0200

    feat: add PWA support

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'singlebackgammon-cache-v1';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) =>
+      fetch(event.request)
+        .then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(() => cache.match(event.request))
+    )
+  );
+});
