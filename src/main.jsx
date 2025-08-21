38ba3ec1ba8d0f4d835fa448eb912608593c0294
nyhave <nyhave@gmail.com>
2025-08-21 09:35:45 +0200

    Configure Vite build for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SingleBackgammon">
       <App />
     </BrowserRouter>
   </React.StrictMode>
